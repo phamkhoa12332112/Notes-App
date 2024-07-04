c05a0522722dee9c86bd587d2434ccb24d2c01a8
ThaiTuan <tuanvowork99@gmail.com>
2024-07-05 02:15:59 +0700

    feature_create_home_screen

---
+++ lib/resources/sizes_manager.dart
@@ -0,0 +1,15 @@
+class SizesManager {
+  static const double s15 = 15.0;
+  static const double s20 = 20.0;
+
+
+  static const double p12 = 12.0;
+  static const double p100 = 100.0;
+
+  static const double w100 = 100.0;
+
+  static const double m10 = 10.0;
+  static const double m100 = 100.0;
+
+  static const double h60 = 60.0;
+}
---
+++ lib/resources/strings_manager.dart
@@ -0,0 +1,6 @@
+class StringsManger {
+  static const String hintText_home = "Ghi chú bạn thêm sẽ xuất hiện tại đây";
+  static const String searchText_home = "Tìm kiếm ghi chú";
+  static const String app_name = "Notes";
+  static const String update_home = "Đã chỉnh sửa 12:09";
+}
