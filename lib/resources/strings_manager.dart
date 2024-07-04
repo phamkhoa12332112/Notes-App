43a4f28657413032717d80a9eb05c7a6c6e85667
ThaiTuan <tuanvowork99@gmail.com>
2024-07-05 04:02:54 +0700

    feature_create_side_bar

--- lib/resources/strings_manager.dart
+++ lib/resources/strings_manager.dart
@@ -3,4 +3,12 @@ class StringsManger {
   static const String searchText_home = "Tìm kiếm ghi chú";
   static const String app_name = "Notes";
   static const String update_home = "Đã chỉnh sửa 12:09";
+
+  static const String noted = "Ghi chú";
+  static const String remind = "Lời nhắc";
+  static const String add_lable = "Tạo nhãn mới";
+  static const String save = "Lưu trữ";
+  static const String delete = "Thùng rác";
+  static const String setting = "Cài đặt";
+  static const String help = "Trợ giúp và phản hồi";
 }
