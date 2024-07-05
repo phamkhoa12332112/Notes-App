4bec491581f8504b7ae8481104a275b5e9c916c7
ThaiTuan <tuanvowork99@gmail.com>
2024-07-06 02:12:01 +0700

    feature create note screen and set navigator for HomeScreen

---
+++ lib/presentation/add_note_screen/AddNoteScreen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tesst/presentation/home/HomeScreen.dart';
+import 'package:tesst/resources/gaps_manager.dart';
+import 'package:tesst/resources/sizes_manager.dart';
+import 'package:tesst/resources/strings_manager.dart';
+
+class AddNoteScreen extends StatefulWidget {
+  const AddNoteScreen({super.key});
+
+  @override
+  State<AddNoteScreen> createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (_) => CupertinoAlertDialog(
+                                    title: const Text(StringsManger.back),
+                                    content: const Text(
+                                        StringsManger.content_dialog),
+                                    actions: [
+                                      CupertinoDialogAction(
+                                          child: const Text(StringsManger.no),
+                                          onPressed: () =>
+                                              Navigator.pop(context)),
+                                      CupertinoDialogAction(
+                                          child: const Text(StringsManger.yes),
+                                          onPressed: () => Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      const HomeScreen())))
+                                    ],
+                                  ),
+                              barrierDismissible: false);
+                        },
+                        padding: const EdgeInsets.all(SizesManager.p10),
+                        icon: const Icon(
+                          size: SizesManager.s30,
+                          Icons.arrow_back,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.push_pin_outlined),
+                      Icon(Icons.notification_add_outlined),
+                      Icon(Icons.save_alt),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            GapsManager.h10,
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.only(left: SizesManager.p10),
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: StringsManger.title,
+                        hintStyle: TextStyle(
+                            color: Colors.grey, fontSize: SizesManager.s30)),
+                  ),
+                  TextField(
+                    controller: _contentController,
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: StringsManger.noted,
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: SizesManager.e10,
+        child: const Icon(Icons.save),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: SizesManager.h60,
+        color: Colors.white60,
+        child: Row(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_box_outlined),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.palette_outlined),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.text_format_outlined),
+                  ),
+                  const Text(StringsManger.update_home),
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pop(
+                    context, [_titleController.text, _contentController.text]);
+              },
+              icon: const Icon(Icons.more_vert),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
