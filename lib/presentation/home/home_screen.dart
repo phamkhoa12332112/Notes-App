cd4f25c8daa691a9e0c72575c438928c9dcedcef
ThaiTuan <tuanvowork99@gmail.com>
2024-07-08 01:54:00 +0700

    feature setting screen and help screen UI

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Sidebar(),
+      drawer: const Sidebar(),
       appBar: AppBar(
         title: TextField(
           decoration: InputDecoration(
